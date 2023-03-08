f2d39a9b69a72f3fcb8f684455b6aea2380d697f
ffehling <fabian@itcomes.de>
2023-03-08 14:42:32 +0200

    deteil view

--- 3D-Printer-Website/pages/Detail view.py
+++ 3D-Printer-Website/pages/Detail view.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
+product_name = "Lion Model"
+
+
 st.title("Here you can get more info about the Product")
+
+st.header("Product Name: "+product_name)
+col_left, col_right = st.columns(2)
+
+col_left.subheader ("Product Image")
+col_left.image("https://files.cults3d.com/uploads/collection/shot_en/131/low_poly_collection_3D_printing_3.jpg", use_column_width=True)
+
+col_right.subheader("Optinoal parameters")
+
+
+col_right.text("Material / PLA")
+col_right.selectbox("Material", ["PLA", "ABS", "PETG", "Wood", "Metal", "Other"], help= "Select the material you want to print with")
+col_right.color_picker("Pick a color")
+pieces = col_right.number_input("Quantity", min_value=1, max_value=100, value=1, help="How many pieces do you want to print?")
+
+
+
