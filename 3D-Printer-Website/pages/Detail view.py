5fabc70c011a4f7a3c342c77a7d9e64cff561fee
Maximilian124 <126184774+Maximilian124@users.noreply.github.com>
2023-03-08 19:42:12 +0200

    changed the content of the Subheader "Optional parameters"

--- 3D-Printer-Website/pages/Detail view.py
+++ 3D-Printer-Website/pages/Detail view.py
@@ -11,13 +11,32 @@ col_left, col_right = st.columns(2)
 col_left.subheader ("Product Image")
 col_left.image("https://files.cults3d.com/uploads/collection/shot_en/131/low_poly_collection_3D_printing_3.jpg", use_column_width=True)
 
-col_right.subheader("Optinoal parameters")
+#col_right.subheader("Optional parameters")
 
 
-col_right.text("Material / PLA")
-col_right.selectbox("Material", ["PLA", "ABS", "PETG", "Wood", "Metal", "Other"], help= "Select the material you want to print with")
-col_right.color_picker("Pick a color")
-pieces = col_right.number_input("Quantity", min_value=1, max_value=100, value=1, help="How many pieces do you want to print?")
+#col_right.text("Material / PLA")
+#col_right.selectbox("Material", ["PLA", "ABS", "PETG", "Wood", "Metal", "Other"], help= "Select the material you want to print with")
+#col_right.color_picker("Pick a color")
+#pieces = col_right.number_input("Quantity", min_value=1, max_value=100, value=1, help="How many pieces do you want to print?")
 
 
+
+st.subheader("Optional parameters")
+# Creating form for the ordering process
+form = st.form("Ordering form")
+
+form.selectbox("Material", ["PLA", "ABS", "PETG", "Wood", "Metal", "Other"], help= "Select the material you want to print with")
+form.color_picker("Pick a color")
+pieces = form.number_input("Quantity", min_value=1, max_value=100, value=1, help="How many pieces do you want to print?")
+#form.form_submit_button("Buy")
+
+if form.form_submit_button("Buy", help="Click here to buy the selected material"):
+        # Prompt the user to confirm the order
+        if st.warning("Do you want to continue ordering?"):
+            # If the user confirms, go to the "Editing Models" page
+            st.experimental_set_query_params(page="editing_models")
+        else:
+            # If the user cancels, go back to the home page
+            st.experimental_set_query_params(page="home")
+            
 
