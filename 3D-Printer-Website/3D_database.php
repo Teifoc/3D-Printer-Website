3785768bf89f3e1bf461b0f669fc7e9e601fc00b
WangXingZhen02 <127110667+WangXingZhen02@users.noreply.github.com>
2023-03-15 00:03:05 +0800

    Update 3D_database.php

--- 3D-Printer-Website/3D_database.php
+++ 3D-Printer-Website/3D_database.php
@@ -1,33 +1,15 @@
 <?php
-// 数据库连接参数
-$dbhost = "database-1.c8eew8ixqxd2.eu-north-1.rds.amazonaws.com";
-$dbport = 3306;
-$dbname = "webside_3Dprint";
-$dbuser = "Wxz";
-$dbpass = "Zhenz021127.";
+    $host = 'your-database-endpoint.rds.amazonaws.com';
+    $username = 'your-username';
+    $password = 'your-password';
+    $dbname = 'your-database-name';
 
-// 创建连接
-$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $dbport);
+    // 创建数据库连接
+    $conn = new mysqli($host, $username, $password, $dbname);
 
-// 检查连接是否成功
-if ($conn->connect_error) {
-    die("连接失败: " . $conn->connect_error);
-}
-
-// 执行查询语句
-$sql = "SELECT * FROM your-table-name-here";
-$result = $conn->query($sql);
-
-// 检查查询结果
-if ($result->num_rows > 0) {
-    // 输出数据
-    while($row = $result->fetch_assoc()) {
-        echo "ID: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    // 检查数据库连接是否成功
+    if ($conn->connect_error) {
+        die("连接失败: " . $conn->connect_error);
     }
-} else {
-    echo "没有结果";
-}
-
-// 关闭连接
-$conn->close();
+    echo "连接成功";
 ?>
