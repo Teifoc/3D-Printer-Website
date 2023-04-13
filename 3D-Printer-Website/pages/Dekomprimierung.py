29907ba68a604ebe61d0878da9affca7da04d0e9
Maximilian124 <126184774+Maximilian124@users.noreply.github.com>
2023-04-13 20:26:20 +0300

    changes

--- 3D-Printer-Website/pages/Dekomprimierung.py
+++ 3D-Printer-Website/pages/Dekomprimierung.py
@@ -15,18 +15,18 @@ db = s.client.get_database('Website')
 collection = db['Models']
 
 # Dummy-Modelle
-# models = [
-#     {
-#         "name": "Modell 1",
-#         "picture": gzip.compress(b"Dummy picture data"),
-#         "stlFile": gzip.compress(b"Dummy STL file data")
-#     },
-#     {
-#         "name": "Modell 2",
-#         "picture": gzip.compress(b"Dummy picture data"),
-#         "stlFile": gzip.compress(b"Dummy STL file data")
-#     }
-# ]
+models = [
+    # {
+    #     "name": "Modell 1",
+    #     "picture": gzip.compress(b"Dummy picture data"),
+    #     "stlFile": gzip.compress(b"Dummy STL file data")
+    # },
+    # {
+    #     "name": "Modell 2",
+    #     "picture": gzip.compress(b"Dummy picture data"),
+    #     "stlFile": gzip.compress(b"Dummy STL file data")
+    # }
+]
 
 # Cache für die Datenbankabfrage
 model_cache = {}
@@ -56,6 +56,83 @@ def decompress_data(compressed_data):
 
 
 # Streamlit-App
+# def main():
+#     st.title("3D-Modelle")
+#
+#     # Modell auswählen
+#     model = select_model(models)
+#     if model is None:
+#         st.error("Kein Modell gefunden!")
+#         return
+#
+#     # Bilddatei dekomprimieren und anzeigen
+#     picture_data = decompress_data(model['picture'])
+#     picture = Image.open(BytesIO(picture_data))
+#     st.image(picture, caption="Bild des 3D-Modells")
+#
+#     # STL-File dekomprimieren und als PyVista-Gitter laden
+#     # st.write("3D-Modell:")
+#     stl_file_data = decompress_data(model['stlFile'])
+#     # mesh = trimesh.load(BytesIO(stl_file_data), file_type="stl")
+#     # grid = pv.PolyData(mesh.vertices, mesh.faces)
+#     #
+#     # # ipythreejs does not support scalar bars :(
+#     # pv.global_theme.show_scalar_bar = False 
+#     #
+#     # # Initialize a plotter object
+#     # plotter = pv.Plotter(window_size=[400,400])
+#     #
+#     # # Add the mesh to the plotter
+#     # plotter.add_mesh(grid, color="white")
+#     #
+#     # # Final touches
+#     # plotter.background_color = "black"
+#     # plotter.view_isometric()
+#     #
+#     # # Pass a key to avoid re-rendering at each time something changes in the page
+#     # stpyvista(plotter, key="document")
+#     #
+#     # # Informationen zum Modell anzeigen
+#     # st.write("Name:", model['name'])
+#     # st.write("Beschreibung:", model['description'])
+#     # st.write("Tags:", model['tags'])
+#     # st.write("Autor:", model['author'])
+#     # st.write("Datum:", model['date'].strftime("%d.%m.%Y"))
+#     #
+#     # # 3D-Ansicht des Modells als STL-File
+#     # st.write("STL-File herunterladen:")
+#     # st.download_button(
+#     #     label="Download",
+#     #     data=stl_file_data,
+#     #     file_name=model['name']+".stl",
+#     #     mime="application/octet-stream"
+#     # )
+#
+#     ## Load the STL file as a Trimesh object
+#     mesh_trimesh = trimesh.load(fileobj=BytesIO(stl_file_data), file_type="stl")
+#
+#     ## Convert the Trimesh object to a PyVista mesh
+#     mesh_pv = pv.PolyData(mesh_trimesh.vertices, mesh_trimesh.faces)
+#
+#     ## Add some scalar field associated to the mesh
+#     mesh_pv['myscalar'] = mesh_pv.points[:, 2]*mesh_pv.points[:, 0]
+#
+#     ## Initialize a plotter object
+#     plotter = pv.Plotter(window_size=[400,400])
+#
+#     ## Add mesh to the plotter
+#     plotter.add_mesh(mesh_pv, scalars='myscalar', cmap='bwr', line_width=1)
+#
+#     ## Final touches
+#     plotter.background_color = "black"
+#     plotter.view_isometric()
+#
+#     ## Pass a key to avoid re-rendering at each time something changes in the page
+#     stpyvista(plotter, key="pv_stl")
+
+
+
+
 def main():
     st.title("3D-Modelle")
 
@@ -70,49 +147,122 @@ def main():
     picture = Image.open(BytesIO(picture_data))
     st.image(picture, caption="Bild des 3D-Modells")
 
-    # STL-File dekomprimieren und als PyVista-Gitter laden
-    st.write("3D-Modell:")
-    stl_file_data = decompress_data(model['stlFile'])
-    mesh = trimesh.load(BytesIO(stl_file_data), file_type="stl")
-    grid = pv.PolyData(mesh.vertices, mesh.faces)
+    # # STL-File dekomprimieren und als PyVista-Gitter laden
+    # stl_file_data = decompress_data(model['stlFile'])
+    # mesh = trimesh.load(BytesIO(stl_file_data), file_type="stl")
+    # grid = pv.PolyData(mesh.vertices, mesh.faces)
+    #
+    # # Add some scalar field associated to the mesh
+    # grid['myscalar'] = grid.points[:, 2]*grid.points[:, 0]
+    #
+    # # Initialize a plotter object
+    # plotter = pv.Plotter(window_size=[400,400])
+    #
+    # # Add mesh to the plotter
+    # plotter.add_mesh(grid, scalars='myscalar', cmap='bwr', line_width=1)
+    #
+    # # Final touches
+    # plotter.background_color = "black"
+    # plotter.view_isometric()
+    #
+    # # Pass a key to avoid re-rendering at each time something changes in the page
+    # stpyvista(plotter, key="pv_stl")
+    #
+    # # Informationen zum Modell anzeigen
+    # st.write("Name:", model['name'])
+    # st.write("Beschreibung:", model['description'])
+    # st.write("Tags:", model['tags'])
+    # st.write("Autor:", model['author'])
+    # st.write("Datum:", model['date'].strftime("%d.%m.%Y"))
+    #
+    # # 3D-Ansicht des Modells als STL-File
+    # st.write("STL-File herunterladen:")
+    # st.download_button(
+    #     label="Download",
+    #     data=stl_file_data,
+    #     file_name=model['name']+".stl",
+    #     mime="application/octet-stream"
+    # )
+    
+    
+    # Binärdaten des ausgewählten Modells abrufen
+    model_data = collection.find_one({"name": model})["stlFile"]
+    
+    # Gzip-komprimierte Daten in ein BytesIO-Objekt laden
+    model_io = io.BytesIO(model_data)
+    with gzip.GzipFile(fileobj=model_io, mode="rb") as f:
+        uncompressed_data = f.read()
+    
+    # STL-Modell aus den unkomprimierten Daten lesen
+    model_file = io.BytesIO(uncompressed_data)
+    mesh = pv.read(model_file, file_format="stl")
+    
+    # Plotter initialisieren
+    plotter = pv.Plotter(window_size=[400, 400])
+    
+    # Mesh zum Plotter hinzufügen
+    plotter.add_mesh(mesh)
+    
+    # Plotter anzeigen
+    stpyvista(plotter)
 
-    # ipythreejs does not support scalar bars :(
-    pv.global_theme.show_scalar_bar = False 
 
-    # Initialize a plotter object
-    plotter = pv.Plotter(window_size=[400,400])
 
-    # Add the mesh to the plotter
-    plotter.add_mesh(grid, color="white")
+# import streamlit as st
+# import pyvista as pv
+# from stpyvista import stpyvista
+# import secret as s
+# import io
+# import gzip
+#
+# # Verbindung zur MongoDB-Datenbank herstellen
+# client = s.client
+# db = client["Website"]
+# models_coll = db["Models"]
+#
+# # Alle Modelle aus der Datenbank abrufen
+# models = list(models_coll.find())
+#
+# # Eine Liste der Modellnamen erstellen
+# model_names = [model["name"] for model in models]
+#
+# # Modell auswählen
+# model_name = st.selectbox("Select a model", model_names)
+#
+# # Binärdaten des ausgewählten Modells abrufen
+# model_data = models_coll.find_one({"name": model_name})["stlFile"]
+#
+# # Gzip-komprimierte Daten in ein BytesIO-Objekt laden
+# model_io = io.BytesIO(model_data)
+# with gzip.GzipFile(fileobj=model_io, mode="rb") as f:
+#     uncompressed_data = f.read()
+#
+# # STL-Modell aus den unkomprimierten Daten lesen
+# model_file = io.BytesIO(uncompressed_data)
+# mesh = pv.read(model_file, file_format="stl")
+#
+# # Plotter initialisieren
+# plotter = pv.Plotter(window_size=[400, 400])
+#
+# # Mesh zum Plotter hinzufügen
+# plotter.add_mesh(mesh)
+#
+# # Plotter anzeigen
+# stpyvista(plotter)
 
-    # Final touches
-    plotter.background_color = "black"
-    plotter.view_isometric()
 
-    # Pass a key to avoid re-rendering at each time something changes in the page
-    stpyvista(plotter, key="document")
 
-    # Informationen zum Modell anzeigen
-    st.write("Name:", model['name'])
-    st.write("Beschreibung:", model['description'])
-    st.write("Tags:", model['tags'])
-    st.write("Autor:", model['author'])
-    st.write("Datum:", model['date'].strftime("%d.%m.%Y"))
 
-    # 3D-Ansicht des Modells als STL-File
-    st.write("STL-File herunterladen:")
-    st.download_button(
-        label="Download",
-        data=stl_file_data,
-        file_name=model['name']+".stl",
-        mime="application/octet-stream"
-    )
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
     main()
- 
- 
  
  
     
@@ -121,8 +271,8 @@ if __name__ == '__main__':
 # import streamlit as st
 # import pyvista as pv
 # from stpyvista import stpyvista
-#
-# # ipythreejs does not support scalar bars :(
+
+# ipythreejs does not support scalar bars :(
 # pv.global_theme.show_scalar_bar = False 
 #
 # ## Initialize a plotter object
---
+++ 3D-Printer-Website/pages/ModelView.py
@@ -0,0 +1,123 @@
+#
+
+# import gzip
+# import tempfile
+# import streamlit as st
+# import pyvista as pv
+# import secret as s
+#
+# # Verbindung zur MongoDB herstellen
+# client = s.client
+# db = client["Website"]
+# collection = db["Models"]
+#
+# # Liste von verfügbaren 3D-Modellen aus der Datenbank abrufen
+# model_names = [doc["name"] for doc in collection.find({}, {"name": 1})]
+#
+# # Dropdown-Menü zur Auswahl eines Modells erstellen
+# model_name = st.selectbox("Wähle ein Modell aus", model_names)
+#
+# # STL-Datei des ausgewählten Modells aus der Datenbank laden und entpacken
+# model_doc = collection.find_one({"name": model_name})
+# stl_bytes = gzip.decompress(model_doc["stlFile"])
+#
+# # Bytes der STL-Datei in eine temporäre Datei schreiben
+# with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+#     temp_file.write(stl_bytes)
+#     temp_file.flush()
+#
+#     # PyVista-Gitter aus der temporären Datei laden
+#     mesh = pv.read(temp_file.name)
+#
+#     # Temporäre Datei löschen
+#     temp_file.unlink(temp_file.name)
+#
+# # 3D-Modell mit PyVista anzeigen
+# plotter = pv.Plotter()
+# plotter.add_mesh(mesh, show_edges=True)
+# plotter.show()
+
+
+
+# import streamlit as st
+# import pyvista as pv
+# from stpyvista import stpyvista
+# import secret as s
+#
+# # Verbindung zur MongoDB-Datenbank herstellen
+# client = s.client
+# db = client["Website"]
+# models_coll = db["Models"]
+#
+#
+#
+# # ipythreejs does not support scalar bars :(
+# pv.global_theme.show_scalar_bar = False 
+#
+# ## Initialize a plotter object
+# plotter = pv.Plotter(window_size=[400,400])
+#
+# ## Create a mesh with a cube 
+# mesh = pv.Cube(center=(0,0,0))
+#
+# ## Add some scalar field associated to the mesh
+# mesh['myscalar'] = mesh.points[:, 2]*mesh.points[:, 0]
+#
+# ## Add mesh to the plotter
+# plotter.add_mesh(mesh, scalars='myscalar', cmap='bwr', line_width=1)
+#
+# ## Final touches
+# plotter.background_color = "black"
+# plotter.view_isometric()
+#
+# ## Pass a key to avoid re-rendering at each time something changes in the page
+# stpyvista(plotter, key="pv_cube")
+
+
+import streamlit as st
+import pyvista as pv
+from stpyvista import stpyvista
+import secret as s
+import io
+import gzip
+
+# Verbindung zur MongoDB-Datenbank herstellen
+client = s.client
+db = client["Website"]
+models_coll = db["Models"]
+
+# Alle Modelle aus der Datenbank abrufen
+models = list(models_coll.find())
+
+# Eine Liste der Modellnamen erstellen
+model_names = [model["name"] for model in models]
+
+# Modell auswählen
+model_name = st.selectbox("Select a model", model_names)
+
+# Binärdaten des ausgewählten Modells abrufen
+model_data = models_coll.find_one({"name": model_name})["stlFile"]
+
+# Gzip-komprimierte Daten in ein BytesIO-Objekt laden
+model_io = io.BytesIO(model_data)
+with gzip.GzipFile(fileobj=model_io, mode="rb") as f:
+    uncompressed_data = f.read()
+
+# STL-Modell aus den unkomprimierten Daten lesen
+model_file = io.BytesIO(uncompressed_data)
+mesh = pv.read(model_file, file_format="stl")
+
+# Plotter initialisieren
+plotter = pv.Plotter(window_size=[400, 400])
+
+# Mesh zum Plotter hinzufügen
+plotter.add_mesh(mesh)
+
+# Plotter anzeigen
+stpyvista(plotter)
+
+
+
+
+
+
