6516d61d8989420945800396ef8fec392add8377
ffehling <fabian@itcomes.de>
2023-04-14 15:47:12 +0300

    test

--- 3D-Printer-Website/pages/ModelView.py
+++ 3D-Printer-Website/pages/ModelView.py
@@ -73,6 +73,32 @@
 # ## Pass a key to avoid re-rendering at each time something changes in the page
 # stpyvista(plotter, key="pv_cube")
 
+import streamlit as st
+import pyvista as pv
+from stpyvista import stpyvista
+
+# pythreejs does not support scalar bars :(
+pv.global_theme.show_scalar_bar = False 
+
+## Initialize a plotter object
+plotter = pv.Plotter(window_size=[400,400])
+
+## Create a mesh with a cube 
+mesh = pv.Cube(center=(0,0,0))
+
+## Add some scalar field associated to the mesh
+mesh['myscalar'] = mesh.points[:, 2]*mesh.points[:, 0]
+
+## Add mesh to the plotter
+plotter.add_mesh(mesh, scalars='myscalar', cmap='bwr', line_width=1)
+
+## Final touches
+plotter.view_isometric()
+plotter.add_scalar_bar()
+plotter.background_color = 'white'
+
+## Pass a key to avoid re-rendering at each time something changes in the page
+stpyvista(plotter, key="pv_cube")
 
 import streamlit as st
 import pyvista as pv
